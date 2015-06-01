6e694860c1baaa0391b5766a0056b8acad0aeaf8
Will Mainwaring <willm@yogainternational.com>
2015-06-01 10:22:11 -0400

    I’ve completed all of the koans. Unfortunately, I didn’t get to the extra credit, but I’ll look forward to doing it later!

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,16 +32,16 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
       var productsICanEat = [];
+ productsICanEat =  _(products).filter(function (noNuts) { return !noNuts.containsNuts && !_(noNuts.ingredients).any(function (noMushrooms) {return noMushrooms === "mushrooms"});});
 
       /* solve using filter() & all() / any() */
-
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,16 +55,24 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
-
+    
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
-  });
+var range1 = _.range(0,1000,3);
+var range2 = _.range(0,1000,5); 
+var range = range1.concat(range2);
+var sum = _(range).chain()
+  .uniq()
+  .reduce(function (memo, x){return memo + x}, 0)
+  .value();
 
+      /* try chaining range() and reduce() */
+    expect(233168).toBe(sum);
+  
+});
   /*********************************************************************************/
    it("should count the ingredient occurrence (imperative)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
@@ -75,39 +83,38 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
+    var count = _(products).chain()
+      .map(function(s) {return s.ingredients})
+      .flatten()
+      .reduce(function(memo,v) {
+        ingredientCount[v] = (ingredientCount[v] || 0) + 1;
+      });
+      console.log(ingredientCount.mushrooms);
+   
+
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
   /*
   it("should find the largest prime factor of a composite number", function () {
-
   });
-
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-
   });
-
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-
-
   });
-
   it("should find the difference between the sum of the squares and the square of the sums", function () {
-
   });
-
   it("should find the 10001st prime", function () {
-
   });
   */
 });
